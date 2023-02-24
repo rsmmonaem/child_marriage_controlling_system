5670d50bd9899bae37791f3460b4dff4e2b35eca
Tanvir Ahmed <tanvirahmed1418@gmail.com>
2023-02-24 14:49:56 +0600

    Removed Zonal and Branch Manager

--- application/views/branch_manager/employee_list.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//include "inc/head_links.php";
-include "inc/form_header_links.php";
-include "inc/left_sidebar.php";
-include "inc/top_bar.php";
-include "inc/employee_list_content.php";
-include "inc/footer.php";
-include "inc/dashboard_footer_js.php";
--- application/views/branch_manager/inc/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    © <?php echo date("Y") ?> All rights reserved || Developed by <a target="_blank" href="https://inleadsit.com">Inleads IT Solution Ltd</a>.
-</footer>
-
-</div>
-<!-- End Right content here -->
-
-</div>
-<!-- END wrapper -->
